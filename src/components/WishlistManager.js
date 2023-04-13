1805a40e214b50701d0f44853054048a0f983e84
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2023-04-13 19:31:51 +0200

    first commit

---
+++ src/components/WishlistManager.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function WishlistManager(props) {
+    return (
+        <div>
+            <header>
+                <h1>Wishlist Manager</h1>
+            </header>
+            <h2>Categories</h2>
+            <h2>Items</h2>
+        </div>
+    );
+}
+
+export default WishlistManager;
