ec5e2b323a4f0dc1be17620ee1418d1aeb1693a8
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2023-04-16 14:53:38 +0200

    Adds Release Date html and some dummy value

--- src/components/WishlistCard.js
+++ src/components/WishlistCard.js
@@ -12,12 +12,14 @@ export default function WishlistCard({data, extra}) {
             <div className={"preview"}>
                 <img src={data.picture.includes("http") ? data.picture : require(`../data/${data.picture}`)}
                      alt={`Preview image of ${data.name}`}/>
-
             </div>
-            <div className={"controls"} style={{marginLeft: "auto"}}>
-                <a href={`https://google.com/search?q=${encodeURIComponent(data.name)}`} target="_blank" rel="noopener noreferrer">
-                    <span role={"img"} aria-label={"Related News"}>🄽</span>
-                </a>
+            <div className={"footer"} style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+                <div className={"release_date"}>{data.releaseDate ? data.releaseDate : 'No Release Date'}</div>
+                <div className={"controls"}>
+                    <a href={`https://google.com/search?q=${encodeURIComponent(data.name)}`} target="_blank" rel="noopener noreferrer">
+                        <span role={"img"} aria-label={"Related News"}>🄽</span>
+                    </a>
+                </div>
             </div>
         </div>
     );
