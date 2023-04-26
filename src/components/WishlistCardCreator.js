3822699364246e5ba2596385965755ce09137e61
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2023-04-26 13:59:38 +0200

    Refactor Button

--- src/components/WishlistCardCreator.js
+++ src/components/WishlistCardCreator.js
@@ -1,7 +1,52 @@
 import React, {useState} from "react";
 import addImage from "../assets/add-image.png";
 import "./WishlistCard.scss";
+import * as PropTypes from "prop-types";
 
+function PreviewUploader(props) {
+    return <div className={"preview"}>
+        <label htmlFor="preview-input">
+            {props.cardData.picture ?
+                <img src={props.cardData.picture} alt="Preview" style={{opacity: "75%"}}/> :
+                <img src={addImage} alt="Upload Card Preview"/>}
+            {props.invalidPicture ? <span style={{color: "white", backgroundColor: "red", fontSize: ".8em"}}>Please upload a picture</span> : null}
+        </label>
+        <input id="preview-input" type="file"
+               onChange={props.onChange} style={{display: "none"}} required
+               onInvalid={props.onInvalid}/>
+    </div>;
+}
+
+PreviewUploader.propTypes = {
+    cardData: PropTypes.shape({}),
+    invalidPicture: PropTypes.bool,
+    onChange: PropTypes.func,
+    onInvalid: PropTypes.func
+};
+
+function CardSaver(props) {
+    return <>
+        <label htmlFor={"save-item"}>
+            <span role={"img"} aria-label={"save-item"}>✅</span>
+        </label>
+        <button id="save-item" type={"submit"} style={{display: "none"}} onSubmit={props.onSubmit}>Submit</button>
+    </>
+}
+
+CardSaver.propTypes = {onSubmit: PropTypes.func}
+
+function CardDeleter(props) {
+    return <span role={"img"} aria-label={"Delete"}
+                 onClick={props.onClick}>❌</span>;
+}
+
+CardDeleter.propTypes = {onClick: PropTypes.func};
+
+function ReleaseDateSelector(props) {
+    return <input type="date" id="date" name="date" onChange={props.onChange}/>;
+}
+
+ReleaseDateSelector.propTypes = {onChange: PropTypes.func};
 export default function WishlistCardCreator(props) {
     const [cardData, setCardDate] = useState({});
     const [invalidPicture, setInvalidPicture] = useState(false);
@@ -23,7 +68,7 @@ export default function WishlistCardCreator(props) {
         };
         reader.readAsDataURL(file);
     };
-
+    const saveCardData = () => props.saveItemCallback(props.id, cardData);
     return (
         <form>
             <fieldset>
@@ -35,28 +80,16 @@ export default function WishlistCardCreator(props) {
                             <option key={category} value={category}>{category}</option>
                         ))}
                     </select>
-                    <div className={"preview"}>
-                        <label htmlFor="preview-input" onInvalid={() => alert('test2')}>
-                            {cardData.picture ?
-                                <img src={cardData.picture} alt="Preview" style={{opacity: "75%"}}/> :
-                                <img src={addImage} alt="Upload Card Preview"/>}
-                            {invalidPicture ? <span style={{color: "white", backgroundColor: "red", fontSize: '.8em'}}>Please upload a picture</span> : null}
-                        </label>
-                        <input id="preview-input" type="file"
-                               onChange={updateCardImage} style={{display: "none"}} required
-                               onInvalid={() => setInvalidPicture(true)}/>
-                    </div>
+                    <PreviewUploader cardData={cardData}
+                                     invalidPicture={invalidPicture}
+                                     onInvalid={() => setInvalidPicture(true)}
+                                     onChange={updateCardImage}/>
 
                     <div className={"footer"} style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
-                        <input type="date" id="date" name="date" onChange={updateReleaseDate}/>
+                        <ReleaseDateSelector onChange={updateReleaseDate}/>
                         <div className={"controls"} style={{display: "flex", gap: "5px"}}>
-                            <span role={"img"} aria-label={"Delete"}
-                                  onClick={() => props.deleteItemCallback(props.id)}>❌</span>
-                            <label htmlFor={"save-item"}>
-                                <span role={"img"} aria-label={"save-item"}>✅</span>
-                            </label>
-                            <button id="save-item" type={"submit"} style={{display: "none"}}
-                                onSubmit={() => props.saveItemCallback(props.id, cardData)}>Submit</button>
+                            <CardDeleter onClick={() => props.deleteItemCallback(props.id)}/>
+                            <CardSaver onSubmit={saveCardData}/>
                         </div>
                     </div>
                 </div>
